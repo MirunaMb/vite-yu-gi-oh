6f1e1b33510c7a3e7e0ee3286957a33d1fe9ffc7
MirunaMb <mirunamarcela98@gmail.com>
2023-09-12 16:50:59 +0200

    create project ,add bootstrap

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data(){
+    return{
+      title: 'Hello'
+    }
+  }
+}
+</script>
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<style lang="scss">
+@use './styles/general.scss'
+</style>
