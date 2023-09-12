291357b94fc0df840f9dee79499e6a0d69d0bed3
MirunaMb <mirunamarcela98@gmail.com>
2023-09-12 18:25:59 +0200

    add components import/export

---
+++ src/components/cards/CardsList.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  data(){
+    return{
+      title: 'Cards'
+    };
+  },
+};
+</script>
+<template>
+  <section>
+    <h1 class="title">{{ title }}</h1>
+  </section>
+    
+ 
+</template>
+<style lang="scss" scoped>
+
+
+
+
+</style>
